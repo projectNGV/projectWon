3315c3590759000cd0a8663637ed9c43d9afbbdb
jongmin1204 <dwdas99@gmail.com>
2025-08-01 01:47:07 +0900

    autopark 수정

--- src/ASW/app/main0.c
+++ src/ASW/app/main0.c
@@ -6,6 +6,8 @@
 #include "bluetooth.h"
 #include "motor.h"
 
+#include "autopark.h"
+
 
 int logging = 0;
 int start = 0;
@@ -26,64 +28,13 @@ int start = 0;
 void main0(){
     systemInit();
     while(1){
-        char command = bluetoothRecvByteNonBlocked();
+        char command = asclin0InUart();
+        asclin0OutUart(command);
 
-        if(command == 'y'){
-            upKp(0,0.001);
+        if(command == 'p'){
+            parkingAutonomousProcess();
         }
 
-
-        if(command == 'u'){
-            upKp(1,0.0001);
-        }
-
-//
-//        if(command == 'i'){
-//            upKp(2,0.0001);
-//        }
-
-
-        if(command == 'h'){
-            upKp(0,-0.001);
-        }
-
-        if(command == 'j'){
-            upKp(1,-0.0001);
-        }
-//
-//        if(command == 'k'){
-//            upKp(2,-0.0001);
-//        }
-
-        if(command == 'o'){
-            start = !start;
-            bluetoothSendByteBlocked(command);
-            if(start){
-                levelInit(LEVEL_LEFT);
-            } else {
-                motorStop();
-            }
-        }
-
-        if(start){
-            int str = steer(LEVEL_LEFT);
-            bluetoothPrintf("steer: %d\n", str);
-        }
-        if(command == 'x'){
-            motorStop();
-            bluetoothSendByteBlocked(command);
-        }
-
-        if(command == 'w'){
-            motorMoveForward(300);
-            bluetoothSendByteBlocked(command);
-        }
-
-        if(command == 's'){
-            motorReverse(300);
-            bluetoothSendByteBlocked(command);
-
-        }
         delayMs(50);
     }
 }
---
+++ src/ASW/autopark/autopark.h
@@ -0,0 +1,11 @@
+#ifndef ASW_AUTOPARK_AUTOPARK_H_
+#define ASW_AUTOPARK_AUTOPARK_H_
+
+#include "ultrasonic.h"
+#include "motor.h"
+#include "util.h"
+#include <stdbool.h>
+
+
+
+#endif /* ASW_AUTOPARK_LEVEL_H_ */
