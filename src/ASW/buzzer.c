b3503995583ac766acc0992732f8415102288151
Jun-hyeok-Park <joonhyeoki@gmail.com>
2025-08-05 16:50:28 +0900

    buzzer modify

--- src/ASW/buzzer.c
+++ src/ASW/buzzer.c
@@ -4,23 +4,24 @@ extern volatile uint16 g_beepInterval;
 
 void buzzerParkingWarning (int rearDistance)
 {
-    if(rearDistance < 500)
+    if (rearDistance <= 150)
+    {
+        Gpt1_Interrupt_Disable();  // 인터럽트 끄고
+        buzzerOn();                // 지속 울림
+    }
+    else if (rearDistance <= 300)
     {
         Gpt1_Interrupt_Enable();
-
-        if(rearDistance < 200)
-        {
-            g_beepInterval = 1000;  // 지속 삐——
-        }
-        if(rearDistance < 400)
-        {
-            g_beepInterval = 3000;  // 빠르게 삐삐삐
-        }
-
-        g_beepInterval = 8000;  // 느리게 삐– 삐–
+        g_beepInterval = 3000;     // 빠르게 삐삐삐삐
+    }
+    else if (rearDistance <= 500)
+    {
+        Gpt1_Interrupt_Enable();
+        g_beepInterval = 8000;     // 느리게 삐–삐–삐
     }
     else
     {
-        Gpt1_Interrupt_Disable();  // 울림 없음
+        Gpt1_Interrupt_Disable();
+        buzzerOff();               // 울림 없음
     }
 }
