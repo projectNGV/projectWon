99880890a9f05055e4bdbc9b5b7cd0e347eba7e0
jongmin1204 <dwdas99@gmail.com>
2025-08-06 11:56:15 +0900

    준혁이형 코드 머지

--- src/app/main0.c
+++ src/app/main0.c
@@ -1,171 +1,41 @@
 #include "main0.h"
 
 #include "ultrasonic.h"
+#include "aeb.h"
 #include "tof.h"
-#include "level.h"
-#include "bluetooth.h"
-#include "motor.h"
+#include "control.h"
+#include "fsm.h"
+#include "led.h"
+#include "buzzerport.h"
 #include "buzzer.h"
-#include "gpt12.h"
 
-#define MAX_STEERING 200
+MotorState motorState = {.baseDuty = 50,      // 사용자 설정 Duty
+        .currentDuty = 0,   // 현재 Duty
+        .currentDir = '5',  // 현재 주행 방향
+        .prevDir = '5',     // 이전 주행 방향
+        .lastKeyInput = '5' // 방금 받은 키보드 입력
+        };
 
-int logging = 0;
-boolean start = FALSE;
+extern volatile boolean g_isLogin;
 
-void main0 ()
+void main0 (void)
 {
     systemInit();
-    myPrintf("SYSTEM START\n");
-    bluetoothPrintf("SYSTEM START\n");
-    float val;
+    myPrintf("System Start\n");
+    g_isLogin = TRUE; // Debugging
+    authenticate();
+
     while (1)
     {
-        char command = asclin0InUartNonBlock();
-
-        if (command != -1)
+        if (motorState.lastKeyInput == 't')
         {
-            myPrintf("COMMAND: %c\n", command);
+            tofOnOff();
+            motorState.lastKeyInput = ' ';
         }
 
-        if (command == 'g')
-        {
-            int speed, round;
-            bluetoothPrintf("Speed(0 ~ 1000): ");
-            bluetoothScanf("%d", &speed);
-            bluetoothPrintf("Round: ");
-            bluetoothScanf("%d", &round);
+        handleStateMachine(&motorState);
 
-            while (round--)
-            {
-                motorMoveForward(speed);
-                delayMs(25);
-                motorStop();
-                delayMs(25);
-            }
-        }
-        if (command == 'z')
-        {
-            auto_park();
-        }
-
-        if (command == 'm')
-        {
-            buzzerToggleOn();
-        }
-        if (command == 'n')
-        {
-
-            buzzerToggleOff();
-        }
-
-        if (command == 'l')
-        {
-            ledToggle(LED_RIGHT);
-
-        }
-        if (command == 'b')
-        {
-            ledToggle(EMPTY);
-
-        }
-
-        if (command == 'c')
-        {
-//            printState();
-            //bluetoothPrintf("left dis: %d\n", getDistanceByUltra(ULT_LEFT));
-           // bluetoothPrintf("rear dis: %d\n", getDistanceByUltra(ULT_REAR));
-            myPrintf("left dis: %d\n", getDistanceByUltra(ULT_LEFT));
-            myPrintf("right dis: %d\n", getDistanceByUltra(ULT_RIGHT));
-            myPrintf("rear dis: %d\n", getDistanceByUltra(ULT_REAR));
-
-        }
-
-        if (command == 'p')
-        {
-            int time;
-            int aDuty;
-            int bDuty;
-
-            bluetoothPrintf("aDuty, bDuty, Time: ");
-            bluetoothScanf("%d %d %d", &aDuty, &bDuty, &time);
-            if (aDuty > bDuty)
-            {
-                motorMovChAPwm(aDuty, 1);
-                motorMovChBPwm(bDuty, 1);
-            }
-            else
-            {
-                motorMovChAPwm(aDuty, 0);
-                motorMovChBPwm(bDuty, 0);
-            }
-            delayMs(time);
-            motorStop();
-        }
-
-        if (command == 'y')
-        {
-            bluetoothPrintf("Set Kp: ");
-            bluetoothScanf("%f", &val);
-            setGain(0, val);
-        }
-
-        if (command == 'u')
-        {
-            bluetoothPrintf("Set Ki: ");
-            bluetoothScanf("%f", &val);
-            setGain(1, val);
-        }
-
-        if (command == 'i')
-        {
-            bluetoothPrintf("Set Kd: ");
-            bluetoothScanf("%f", &val);
-            setGain(2, val);
-        }
-
-        if (command == 'o')
-        {
-            start = !start;
-            if (start)
-            {
-                motorMovChAPwm(300, 1);
-                motorMovChBPwm(300, 1);
-                levelInit(LEVEL_LEFT);
-                delayMs(50);
-            }
-            else
-            {
-                motorStop();
-            }
-        }
-
-        if (start)
-        {
-            int mv = getMv(LEVEL_LEFT);
-            if (mv < -MAX_STEERING)
-                mv = -MAX_STEERING;
-            if (mv > MAX_STEERING)
-                mv = MAX_STEERING;
-            bluetoothPrintf("%d\n", mv);
-            motorMovChAPwm(300 + mv, 1);
-            motorMovChBPwm(300 - mv, 1);
-            delayMs(50);
-        }
-        if (command == 'x')
-        {
-            motorStop();
-            start = FALSE;
-        }
-
-        if (command == 'w')
-        {
-            motorMoveForward(300);
-        }
-
-        if (command == 's')
-        {
-            motorMoveReverse(300);
-        }
+//        myPrintf("distance : %d mm,   flag : %d,    duty : %d\n", tofGetValue(), aebFlag, motorState.currentDuty);
+//        delayMs(500);
     }
 }
